ec6d8ffbad0542d5268e8922a9692f644183eaab
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2025-02-14 03:13:00 +0100

    Update banner.tsx
    Fix spelling mistake and making support request more general

--- components/containers/banner/banner.tsx
+++ components/containers/banner/banner.tsx
@@ -4,8 +4,8 @@ export const Banner = () => {
   return (
     <section className="flex justify-center bg-foreground p-2">
       <span className="text-center text-sm font-light text-muted/70">
-        Welcome to the Beta release of The Grid&apos;s profile manger. This is
-        only the start. Questions and feedback to be sent via Intercom.
+        Welcome to the Beta release of The Grid&apos;s profile manager. This is
+        only the start... Open a support chat for questions or feedback.
       </span>
     </section>
   );
